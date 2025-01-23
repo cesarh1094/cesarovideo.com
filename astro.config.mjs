d4fc28c0e99c8410543a1f7cb723c7633f67445f
Cesar Hernandez <cesar@mosaicstg.com>
2025-01-23 17:06:05 -0500

    fix: update Astro and install new tailwind packages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,16 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import qwikdev from "@qwikdev/astro";
+import tailwindcss from '@tailwindcss/vite'
 
 import solidJs from "@astrojs/solid-js";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://example.com",
+  site: "https://cesarovideo.com",
+  vite: {
+    plugins: [tailwindcss()]
+  },
   integrations: [
     mdx(),
     sitemap(),
